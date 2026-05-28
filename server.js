b22e29c0a7d2b5ddd677c182d861085dc3371c60
mdovudov894-art <mdovudov894@gmail.com>
2026-05-28 13:45:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -36,12 +36,11 @@ io.use((socket, next) => {
     try {
         const token = socket.handshake.auth.token;
         if (!token) return next(new Error('Токен нест!'));
-        
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        socket.username = decoded.username; // Номи тасдиқшуда аз Токен
+        socket.username = decoded.username;
         next();
     } catch (err) {
-        return next(new Error('Токен нодуруст аст!'));
+        next(new Error('Токен нодуруст!'));
     }
 });
 
@@ -49,100 +48,73 @@ io.on('connection', (socket) => {
     const username = socket.username;
     console.log('👤 Пайваст шуд:', username);
 
-    // Ба хонаи шахсӣ даромадан
+    // Ба хонаи худаш дохил шав
     socket.join(username);
     onlineUsers.set(username, socket.id);
 
-    // Ба ҳама хабар деҳ, ки онлайн шуд
+    // Ба ҳама хабар деҳ
     io.emit('userOnline', { username });
-
-    // Рӯйхати онлайнҳоро ба худи корбар фиристед
+    // Рӯйхати онлайнҳо
     socket.emit('onlineList', Array.from(onlineUsers.keys()));
 
-    // Фиристодани паём (Амният ислоҳ шуд)
     socket.on('sendMessage', (data) => {
         try {
-            // Номи фиристандаро аз сокети тасдиқшуда мегирем, на аз маълумоти клиент
-            const verifiedSender = socket.username; 
-            
-            io.to(data.receiver).emit('newMessage', {
-                ...data,
-                sender: verifiedSender
-            });
-            io.to(verifiedSender).emit('newMessage', {
-                ...data,
-                sender: verifiedSender
-            });
-        } catch (err) {}
+            // Ба гиранда фиристед
+            io.to(data.receiver).emit('newMessage', data);
+            // Ба фиристандаи дигар device ҳам фиристед
+            io.to(data.sender).emit('newMessage', data);
+        } catch (err) {
+            console.log('sendMessage хатогӣ:', err);
+        }
     });
 
-    // Реаксия (Амният ислоҳ шуд)
     socket.on('reaction', (data) => {
         try {
-            const verifiedSender = socket.username;
-
-            io.to(data.msgReceiver).emit('messageReaction', {
-                ...data,
-                sender: verifiedSender // Амният: фиристандаи аслӣ
-            });
-            io.to(data.msgSender).emit('messageReaction', {
-                ...data,
-                sender: verifiedSender // Амният: фиристандаи аслӣ
-            });
-        } catch (err) {}
+            io.to(data.receiver).emit('reactionUpdate', data);
+            io.to(data.sender).emit('reactionUpdate', data);
+        } catch (err) {
+            console.log('reaction хатогӣ:', err);
+        }
     });
 
-    // Нест кардани паём (Амният ислоҳ шуд)
     socket.on('deleteMessage', (data) => {
         try {
-            const verifiedSender = socket.username;
-
-            io.to(data.receiver).emit('messageDeleted', {
-                ...data,
-                deletedBy: verifiedSender // Амният
-            });
-            io.to(verifiedSender).emit('messageDeleted', {
-                ...data,
-                deletedBy: verifiedSender // Амният
-            });
-        } catch (err) {}
+            io.to(data.receiver).emit('messageDeleted', data);
+            io.to(data.sender).emit('messageDeleted', data);
+        } catch (err) {
+            console.log('deleteMessage хатогӣ:', err);
+        }
     });
 
-    // Навишта истодааст (Амният ислоҳ шуд)
     socket.on('typing', (data) => {
         try {
-            io.to(data.receiver).emit('userTyping', { sender: socket.username });
+            io.to(data.receiver).emit('userTyping', { sender: data.sender });
         } catch (err) {}
     });
 
-    // Навиштанро бас кард (Амният ислоҳ шуд)
     socket.on('stopTyping', (data) => {
         try {
-            io.to(data.receiver).emit('userStopTyping', { sender: socket.username });
+            io.to(data.receiver).emit('userStopTyping', { sender: data.sender });
         } catch (err) {}
     });
 
-    // Дида шудани паём
     socket.on('messageSeen', (data) => {
         try {
             io.to(data.sender).emit('messageSeenUpdate', { msgId: data.msgId });
         } catch (err) {}
     });
 
-    // Иваз кардани номи корбарӣ
     socket.on('changeUsername', (data) => {
         try {
             const { oldUsername, newUsername } = data;
-            // Танҳо худи корбар метавонад номи худро иваз кунад
-            if (socket.username !== oldUsername) return;
-
+            // Хонаро иваз кун
             socket.leave(oldUsername);
             socket.join(newUsername);
             socket.username = newUsername;
-            
+            // onlineUsers навсозӣ
             onlineUsers.delete(oldUsername);
             onlineUsers.set(newUsername, socket.id);
-            
+            // Ба ҳама хабар деҳ
             io.emit('userOffline', { username: oldUsername });
             io.emit('userOnline', { username: newUsername });
             io.emit('usernameChanged', { oldUsername, newUsername });
@@ -160,5 +132,7 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`🚀 Сервер дар порти ${PORT} кор карда истодааст...`);
+    console.log(`🚀 Сервер дар http://localhost:${PORT} кор мекунад`);
 });
+
+module.exports = { io };
